17b692437463a3b195810af0125998716dc94828
karthik <karthik.r@concertidc.com>
2024-04-01 18:51:10 +0530

    Delete page.tsx

--- app/(screens)/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-import { CarouselDemo } from '@/app/components/ui/starter/CarouselDemo';
-
-export default function Page() {
-  return (
-    <div className="m-5 flex max-w-4xl flex-col items-center justify-center px-10 align-middle">
-      <CarouselDemo />
-    </div>
-  );
-}
